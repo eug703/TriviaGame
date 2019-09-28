658811e61c8300ffd7d4b61c170f0da06cd46e22
Eugene Lee <eug703@gmail.com>
2019-09-27 21:07:49 -0400

    Good progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,60 @@
-console.log("$")
+let counter = 5;
+let currentQuestion = 0;
+let score = 0;
+let loss = 0;
+let timer;
+
+function nextQuestion(){
+    const outOfQuestions = (quizQuestions.length - 1) === currentQuestion;
+    if(outOfQuestions){
+        console.log("game over");
+    }
+    else{
+        currentQuestion++;
+        loadQuestion();
+    }
+}
+
+function timeUp(){
+    clearInterval(timer);
+    loss++;
+    nextQuestion();
+}
+
+function countDown(){
+    counter--;
+    $("#time").html("Timer: " + counter);
+
+    if(counter === 0){
+        timeUp();
+    }
+}
+
+
+function loadQuestion() {
+    counter = 5;
+    timer = setInterval(countDown, 1000);
+
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].choices;
+
+
+    $("#time").html("Timer: " + counter);
+    $("#game").html(` 
+        <h3>${question}</h3>
+        ${loadChoices(choices)}
+    `);
+    // $("#game").html("<h3>" + question + "</h3>");
+}
+
+function loadChoices(choices){
+    let result = "";
+
+    for (let i = 0; i < choices.length; i++){
+        result += `<p class = "choice" data-answer="${choices[i]}"> ${choices[i]} </p>`;
+    }
+    return result;
+
+}
+
+loadQuestion();
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,1 +1,21 @@
 console.log("question javascript file link check")
+
+const quizQuestions = [
+    {
+    question: "Which city has Eugene never called home?",
+    choices: ["Oklahoma City", "McLean", "New York City", "Los Angeles"],
+    correctAnswer: "Los Angeles"
+    },
+
+    {
+    question: "How did Eugene fracture his shin in second grade?",
+    choices: ["Soccer", "A vicious Razor scooter", "Falling off a bike", "Jumping off the top level of the playground set"],
+    correctAnswer: "Soccer"
+    },
+
+    {
+    question: "Who is Eugene's favorite staff member?",
+    choices: ["Gary", "Nolan", "Erik", "All of the above"],
+    correctAnswer: "All of the above"
+    }
+];
